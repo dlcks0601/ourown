f5dcd5ea86c6ab7afef590c7f13d46835cbe7904
leechan <dlcks0601@gmail.com>
2025-06-26 00:19:29 +0900

    [Refactor] PartnerStatus 컴포넌트를 CoupleMemo로 교체: 홈 화면에서 파트너 상태 표시를 메모로 변경

--- src/app/(protected)/(tabs)/(home)/index.tsx
+++ src/app/(protected)/(tabs)/(home)/index.tsx
@@ -1,6 +1,6 @@
 import CoupleLink from '@/components/CoupleLink';
+import CoupleMemo from '@/components/CoupleMemo';
 import CoupleWidget from '@/components/CoupleWidget';
-import PartnerStatus from '@/components/PartnerStatus';
 import Todo from '@/components/Todo';
 import React from 'react';
 import { useColorScheme, View } from 'react-native';
@@ -18,7 +18,7 @@ export default function IndexScreen() {
           <CoupleWidget />
           <Todo />
           <View className='flex-row w-full gap-4'>
-            <PartnerStatus />
+            <CoupleMemo />
             <CoupleLink />
           </View>
         </View>
