c8a89ad2c0a7db31318a50970b79d96291caf175
leechan <dlcks0601@gmail.com>
2025-06-26 00:18:58 +0900

    [Refactor] 색상 상수 및 날짜 포맷 함수 이름 변경: Color.ts에서 색상 값 업데이트 및 Day.ts에서 함수 이름 간소화

--- src/constants/Day.ts
+++ src/constants/Day.ts
@@ -6,7 +6,7 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-export function formatAnniversaryToKoreanDate(utcDateString: string): string {
+export function formatToKoreanDate(utcDateString: string): string {
   return dayjs(utcDateString)
     .tz('Asia/Seoul') // 한국 시간대 적용
     .locale('ko') // 한국어 요일
