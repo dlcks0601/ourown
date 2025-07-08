6f6e9ae026cf4c69557632d53749a3f661e7730c
leechan <dlcks0601@gmail.com>
2025-07-08 21:16:46 +0900

    [Feat] 기념일 D-day 계산 기능 추가: 주어진 기념일 날짜로부터 D-day를 계산하는 함수 구현 및 날짜 포맷 수정

--- src/constants/Day.ts
+++ src/constants/Day.ts
@@ -10,5 +10,24 @@ export function formatToKoreanDate(utcDateString: string): string {
   return dayjs(utcDateString)
     .tz('Asia/Seoul') // 한국 시간대 적용
     .locale('ko') // 한국어 요일
-    .format('YYYY.MM.DD (dd)');
+    .format('YYYY.MM.DD');
+}
+
+export function calculateDday(anniversaryDate: string): number {
+  const anniversary = new Date(anniversaryDate);
+  const today = new Date();
+
+  const startDate = new Date(
+    anniversary.getFullYear(),
+    anniversary.getMonth(),
+    anniversary.getDate()
+  );
+  const endDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+
+  const timeDiff = endDate.getTime() - startDate.getTime();
+  return Math.floor(timeDiff / (1000 * 60 * 60 * 24)) + 1;
 }
