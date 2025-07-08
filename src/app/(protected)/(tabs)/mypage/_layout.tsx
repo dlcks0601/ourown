88eb31dec7c0a87173e36331e6a82bdb5c274935
leechan <dlcks0601@gmail.com>
2025-07-08 21:19:20 +0900

    [Feat] 마이페이지 레이아웃 추가: 새로운 Stack 구성 요소를 사용하여 마이페이지 및 설정 화면의 네비게이션 구조 구현

---
+++ src/app/(protected)/(tabs)/mypage/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack, usePathname } from 'expo-router';
+
+export default function Layout() {
+  const pathname = usePathname();
+
+  return (
+    <Stack
+      screenOptions={{
+        animation: pathname.startsWith('/mypage') ? 'default' : 'none',
+      }}
+    >
+      <Stack.Screen
+        name='index'
+        options={{ title: 'Mypage', headerShown: false }}
+      />
+      <Stack.Screen
+        name='setting'
+        options={{ title: 'Setting', headerShown: false }}
+      />
+      <Stack.Screen
+        name='start-setting'
+        options={{ title: 'StartSetting', headerShown: false }}
+      />
+      <Stack.Screen
+        name='nickname-setting'
+        options={{ title: 'Nickname Setting', headerShown: false }}
+      />
+    </Stack>
+  );
+}
