1b1f392958bca4e6ff2d74a6bda4071364f0b3c0
leechan <dlcks0601@gmail.com>
2025-07-10 02:32:59 +0900

    [Feat] 마이페이지 기념일 API 기능 추가: 기념일 추가, 수정, 삭제를 위한 API 메서드 구현

--- src/api/mypage/mypage.api.ts
+++ src/api/mypage/mypage.api.ts
@@ -10,3 +10,50 @@ export const getMypageAnniversary =
     console.log(response.data);
     return response.data;
   };
+
+export const postMypageAnniversary = async (
+  coupleId: number,
+  title: string,
+  date: string
+) => {
+  const response = await fetcher<mypageAnniversaryResponse>({
+    url: `/couple/${coupleId}/anniversaries`,
+    method: 'POST',
+    data: {
+      coupleId,
+      title,
+      date,
+    },
+  });
+  return response.data;
+};
+
+export const deleteMypageAnniversary = async (coupleId: number, id: number) => {
+  const response = await fetcher<mypageAnniversaryResponse>({
+    url: `/couple/${coupleId}/anniversaries`,
+    method: 'DELETE',
+    data: {
+      id,
+    },
+  });
+  return response.data;
+};
+
+export const patchMypageAnniversary = async (
+  coupleId: number,
+  id: number,
+  title: string,
+  date: string
+) => {
+  const response = await fetcher<mypageAnniversaryResponse>({
+    url: `/couple/${coupleId}/anniversaries`,
+    method: 'PUT',
+    data: {
+      coupleId,
+      id,
+      title,
+      date,
+    },
+  });
+  return response.data;
+};
